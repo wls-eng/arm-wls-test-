ca864cf93a193dfa05bfce3af27eb9dc71a9c1ff
SMANTOOR <SMANTOOR@SMANTOOR-7320.idc.oracle.com>
2022-06-02 11:56:29 +0530

    Added datasource, security, demo-cert and custom-cert tests

--- test_templates/scripts/runAdminOfferTest.sh
+++ test_templates/scripts/runAdminOfferTest.sh
@@ -7,6 +7,7 @@ sudo yum install -y git jq
 sudo mkdir -p /u01/wlstest
 sudo chown -R oracle:oracle /u01/wlstest
 cd /u01/wlstest
+echo runuser -l oracle -c "cd /u01/wlstest ; git clone $testArtifactRepo"
 runuser -l oracle -c "cd /u01/wlstest ; git clone $testArtifactRepo"
 testWorkDir=/u01/wlstest/arm-wls-test
 
@@ -37,6 +38,21 @@ do
 			echo "Executing datasource tests"
 			echo "sh $testWorkDir/datasource/datasource_test.sh -i $testInputFileName"
 			sudo sh $testWorkDir/datasource/datasource_test.sh -i $testInputFileName > ${test}.log 2>&1
+			;;
+	"ssl-customcert")
+			echo "Executing ssl with custom certificates"
+			echo "sh $testWorkDir/ssl/ssl_test.sh -i $testInputFileName -o CERT_TYPE=CUSTOMCERT"
+			sudo sh $testWorkDir/ssl/ssl_test.sh -i $testInputFileName -o CERT_TYPE=CUSTOMCERT > ${test}.log 2>&1
+			;;
+	"ssl-democert")
+			echo "Executing ssl with demo certificates"
+			echo "sh $testWorkDir/ssl/ssl_test.sh -i $testInputFileName -o CERT_TYPE=DEMOCERT"
+			sudo sh $testWorkDir/ssl/ssl_test.sh -i $testInputFileName -o CERT_TYPE=DEMOCERT > ${test}.log 2>&1
+			;;
+	"security")
+			echo "Executing security tests"
+			echo "sh $testWorkDir/security/security_test.sh  -i $testInputFileName"
+			sudo sh $testWorkDir/security/security_test.sh  -i $testInputFileName > ${test}.log 2>&1
 			;;
 	*)
 			usage
