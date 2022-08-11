5c4062cbdf975b3d70421c929b0bf90ac8b67103
gnsuryan <gurudutt.suryanarayana@oracle.com>
2022-08-11 12:42:44 +0000

    modified coherence test case to use WLST instead of REST API

--- coherence/coherence_test.sh
+++ coherence/coherence_test.sh
@@ -5,116 +5,163 @@ DATASOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 source ${DATASOURCE_DIR}/../utils/utils.sh
 source ${DATASOURCE_DIR}/../utils/test_config.properties
 
-function verifyCoherenceCluster()
+function verifyCoherenceClusterName()
 {
-    print "verifying Coherence Cluster..."
+    print "verifying Coherence Cluster Name..."
 
-    output=$(curl -s \
-    --user ${WLS_USERNAME}:${WLS_PASSWORD} \
-    -H X-Requested-By:MyClient \
-    -H Accept:application/json \
-    -H Content-Type:application/json \
-    -X GET ${HTTP_ADMIN_URL}/management/coherence/latest/clusters/${COHERENCE_CLUSTER_NAME})
+cat << EOF >> $SCRIPT_DIR/verifyCoherenceClusterName.py
 
-    print "$output"
+connect("${WLS_USERNAME}","${WLS_PASSWORD}","${T3_ADMIN_URL}")
+cd("/CoherenceClusterSystemResources/${COHERENCE_CLUSTER_NAME}/CoherenceClusterResource/${COHERENCE_CLUSTER_NAME}")
+coherenceClusterName=cmo.getName()
 
-    coherenceClusterName=$(echo $output | jq -r '.clusterName')
+print 'coherence cluster: '+coherenceClusterName
 
-    print "coherence Cluster Name: $coherenceClusterName"
+if coherenceClusterName != "${COHERENCE_CLUSTER_NAME}":
+  print 'Coherence Cluster Name verification failed'
+  raise Exception('Coherence Cluster Name verification failed')
+else:
+  print 'Coherence Cluster Name verification complete'
 
-    if [ "$coherenceClusterName" != "${COHERENCE_CLUSTER_NAME}" ];
-    then
-        echo "FAILURE: Coherence Cluster name verification failed"
-        notifyFail
-    else
-        echo "SUCCESS: Coherence Cluster name verification is successful."
-        notifyPass
-    fi
+disconnect()
+exit()
 
-    coherenceClusterStatus=$(echo $output | jq -r '.running')
+EOF
 
-    if [ "$coherenceClusterStatus" != "true" ];
-    then
-        echo "FAILURE: Coherence Cluster status verification failed"
-        notifyFail
-    else
-        echo "SUCCESS: Coherence Cluster status verification is successful."
-        notifyPass
-    fi
-
-    EXPECTED_COHERENCE_CLUSTER_SIZE=$((${DEFAULT_CLUSTER_SIZE}+${DEFAULT_NUM_OF_COHERENCE_SERVERS}))
-    print "Expected Coherence Cluster size: $EXPECTED_COHERENCE_CLUSTER_SIZE"
-
-    ACTUAL_COHERENCE_CLUSTER_SIZE=$(echo $output | jq -r '.clusterSize')
-    print "Actual Coherence Cluster size: $ACTUAL_COHERENCE_CLUSTER_SIZE"
-
-    if [ "${EXPECTED_COHERENCE_CLUSTER_SIZE}" != "${ACTUAL_COHERENCE_CLUSTER_SIZE}" ];
-    then
-        echo "FAILURE: Coherence Cluster size verification failed"
-        notifyFail
-    else
-        echo "SUCCESS: Coherence Cluster size verification is successful."
-        notifyPass
-    fi
-
-
-    clusterMembers=()
-    output="$(echo $output | jq -r '.members[]')"
-    IFS=
-    while read -r member;
-    do
-        member=$(echo $member | cut -d "(" -f2 | cut -d ")" -f1)
-        member=$(echo $member | grep member | cut -d ',' -f8| cut -d':' -f2)
-        clusterMembers+=($member)
-    done <<< "${output}"
-
-    print "this clusterMembers: ${clusterMembers[*]}"
-    failed=false
-
-    for ((i=1;i<=DEFAULT_CLUSTER_SIZE;i++));
-    do
-       expectedMemberId="${MANAGED_SERVER_PREFIX}${i}";
-
-       if printf '%s\n' ${clusterMembers[@]} | grep -q -P "^${expectedMemberId}$"; then
-            print "Member ${expectedMemberId} part of Coherence cluster"
-       else
-            echo "FAILURE: Coherence Cluster Member verification failed. ${expectedMemberId} not part of Coherence Cluster"
-            failed=true
-            break
-       fi
-    done
-
-    for ((i=1;i<=DEFAULT_NUM_OF_COHERENCE_SERVERS;i++));
-    do
-       expectedMemberId="${COHERENCE_SERVER_PREFIX}${i}";
-
-       if printf '%s\n' ${clusterMembers[@]} | grep -q -P "^${expectedMemberId}$"; then
-            print "Member ${expectedMemberId} part of Coherence cluster"
-       else
-            echo "FAILURE: Coherence Cluster Member verification failed. ${expectedMemberId} not part of Coherence Cluster"
-            failed=true
-            break
-       fi
-    done
-
-
-    if [ "$failed" != "true" ];
-    then
-         echo "SUCCESS: Coherence Cluster Member verification successful."
-         notifyPass
-    else
-         notifyFail
-    fi
+runuser -l oracle -c ". ${ORACLE_HOME}/oracle_common/common/bin/setWlstEnv.sh; java weblogic.WLST ${SCRIPT_DIR}/verifyCoherenceClusterName.py ${REDIRECT_OUTPUT}"
+if [[ $? != 0 ]]; then
+  echo "FAILURE: Coherence Cluster name verification failed"
+  notifyFail
+else
+  echo "SUCCESS: Coherence Cluster name verification is successful."
+  notifyPass
+fi
 
 }
 
+function verifyCoherenceClusterMemberSize()
+{
+    print "verifying Coherence Cluster MemberSize..."
+
+EXPECTED_COHERENCE_CLUSTER_SIZE=$((${DEFAULT_CLUSTER_SIZE}+${DEFAULT_NUM_OF_COHERENCE_SERVERS}))
+print "Expected Coherence Cluster size: $EXPECTED_COHERENCE_CLUSTER_SIZE"
+
+cat << EOF >> $SCRIPT_DIR/verifyCoherenceClusterMemberSize.py
+
+connect("${WLS_USERNAME}","${WLS_PASSWORD}","${T3_ADMIN_URL}")
+cd("/CoherenceClusterSystemResources/${COHERENCE_CLUSTER_NAME}/Targets")
+memberListMap=ls(pwd(), returnMap='true')
+
+if len(memberListMap) != ${EXPECTED_COHERENCE_CLUSTER_SIZE} :
+  print "Coherence Cluster size not matching with expected value"
+  raise Exception('Coherence Cluster size not matching with expected value')
+else:
+  print "Coherence Cluster size matching with expected value"
+
+disconnect()
+exit()
+
+EOF
+
+runuser -l oracle -c ". ${ORACLE_HOME}/oracle_common/common/bin/setWlstEnv.sh; java weblogic.WLST ${SCRIPT_DIR}/verifyCoherenceClusterMemberSize.py ${REDIRECT_OUTPUT}"
+if [[ $? != 0 ]]; then
+  echo "FAILURE: Coherence Cluster size verification failed"
+  notifyFail
+else
+  echo "SUCCESS: Coherence Cluster size verification is successful."
+  notifyPass
+fi
+
+}
+
+function verifyCoherenceClusterMemberList()
+{
+   expectedMemberList=""
+
+   print "verifying Coherence Cluster MemberList..."
+
+   for ((i=1;i<=DEFAULT_CLUSTER_SIZE;i++));
+   do
+       expectedMemberId="${MANAGED_SERVER_PREFIX}${i}"
+       if [ -z "${expectedMemberList}" ];
+       then
+          expectedMemberList="${expectedMemberId}"
+       else
+          expectedMemberList="${expectedMemberList},${expectedMemberId}"
+       fi
+   done
+
+   for ((i=1;i<=DEFAULT_NUM_OF_COHERENCE_SERVERS;i++));
+   do
+       expectedMemberId="${COHERENCE_SERVER_PREFIX}${i}"
+       if [ -z "${expectedMemberList}" ];
+       then
+          expectedMemberList="${expectedMemberId}"
+       else
+          expectedMemberList="${expectedMemberList},${expectedMemberId}"
+       fi
+   done
+
+   print "expected MemberList: ${expectedMemberList}"
+
+cat << EOF >> $SCRIPT_DIR/verifyCoherenceClusterMemberList.py
+
+connect("${WLS_USERNAME}","${WLS_PASSWORD}","${T3_ADMIN_URL}")
+cd("/CoherenceClusterSystemResources/${COHERENCE_CLUSTER_NAME}/Targets")
+memberListMap=ls(pwd(), returnMap='true')
+
+expectedMemberListString="${expectedMemberList}"
+expectedMemberList=list(expectedMemberListString.split(","))
+
+if len(expectedMemberList) != len(memberListMap):
+  raise Exception("Expected Member List and Actual Member List differ")
+
+for x in expectedMemberList:
+  if x not in memberListMap:
+     raise Exception("Expected Member List and Actual Member List differ")
+
+print 'Expected Member List and Actual Member list are same'
+
+disconnect()
+exit()
+
+EOF
+
+runuser -l oracle -c ". ${ORACLE_HOME}/oracle_common/common/bin/setWlstEnv.sh; java weblogic.WLST ${SCRIPT_DIR}/verifyCoherenceClusterMemberList.py ${REDIRECT_OUTPUT}"
+if [[ $? != 0 ]]; then
+  echo "FAILURE: Coherence Cluster List verification failed"
+  notifyFail
+else
+  echo "SUCCESS: Coherence Cluster List verification is successful."
+  notifyPass
+fi
+
+}
 
 #main
+
+ORACLE_HOME="/u01/app/wls/install/oracle/middleware/oracle_home"
+SCRIPT_DIR="/tmp/script"
+
+if [ "${DEBUG}" == "true" ];
+then
+  REDIRECT_OUTPUT=""
+else
+  REDIRECT_OUTPUT=">\/dev\/null 2>&1"
+fi
+
+
+mkdir -p ${SCRIPT_DIR}
+rm -rf ${SCRIPT_DIR}/*.py
 
 get_param "$@"
 
 validate_input
 
-verifyCoherenceCluster
+verifyCoherenceClusterName
+
+verifyCoherenceClusterMemberSize
+
+verifyCoherenceClusterMemberList
 
 printTestSummary
