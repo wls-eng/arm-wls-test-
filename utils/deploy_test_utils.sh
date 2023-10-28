55c4dfdf699bab989fa0b728ca2e416f24fd4064
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2023-10-28 20:50:00 +0530

    Updating pids values
    Signed-off-by: Sanjay Mantoor <sanjay.mantoor@oracle.com>

--- utils/deploy_test_utils.sh
+++ utils/deploy_test_utils.sh
@@ -74,6 +74,18 @@ function hardCodePIDs()
 			sed -i "s|$pidString|$value|g" $jsonFile 
 		done
 	done
+	pidKeys=`cat weblogic-azure-vm/arm-oraclelinux-wls/src/main/resources/azure-common.properties | cut -f1 -d"=" | grep -v '#' | grep -v "^$"`
+	for pidKey in $pidKeys
+	do
+		value=`cat weblogic-azure-vm/arm-oraclelinux-wls/src/main/resources/pid.properties | grep -w "^$pidKey" | cut -f2 -d"="`
+		pidString='${'${pidKey}'}'
+		jsonFiles=`find . -name *.json`
+		for jsonFile in $jsonFiles
+		do
+			echo "Replacing $pidString with $value in $jsonFile"
+			sed -i "s|$pidString|$value|g" $jsonFile 
+		done
+	done
 	git status
 	checkSuccess $? "Git status failed"
 }
